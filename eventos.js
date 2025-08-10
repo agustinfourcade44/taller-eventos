b7a0ea1441aeb3419df1a6eb45476b8b6c2c21ae
Agustin Fourcade <Th4nke5@gmail.com>
2025-08-10 16:06:46 -0300

    Agregado manejador de eventos para el div

---
+++ eventos.js
@@ -0,0 +1,8 @@
+const div = document.getElementById("contenedor");
+
+div.style.padding = "5 px";
+div.style.backgroundColor= "green";
+
+div.addEventListener("click", function() {
+  alert("Hola! Soy el div");
+});
